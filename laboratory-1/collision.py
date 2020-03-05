9420477479427514195be83919d9e64db7553329
Vladislav Veluga <vladislav805@yandex.com>
2020-03-05 18:25:11 +0300

    Исправление вероятностей в collision.py

--- laboratory-1/collision.py
+++ laboratory-1/collision.py
@@ -1,9 +1,15 @@
 import numpy as np;
 from matplotlib import pyplot as plt;
 import random;
+import decimal
+
+def drange(x, y, jump):
+  while x < y:
+    yield float(x)
+    x += decimal.Decimal(jump)
 
 # Рамки Лямбды Пуассона
-ranges = (.01, .3);
+ranges = (.01, .30);
 
 # Шаг моделирования
 step = 0.01;
@@ -13,6 +19,12 @@ rangesScale = 1 / step;
 # Генерация лямбд
 lambdas = [i / rangesScale for i in range(int(ranges[0] * rangesScale), int(ranges[1] * rangesScale), int(step * rangesScale))];
 
+def generateVectorMessages(p, count):
+    res = [];
+    for i in range(count):
+        res.append(1 if random.random() <= p else 0);
+    return res;
+
 # Функция для моделирования процесса с "глупыми" абонентами
 def solve(slots, clients):
     graph = [];
@@ -20,8 +32,8 @@ def solve(slots, clients):
         lost = 0;
 
         for slot in range(1, slots):
-            clientsInterFlow = np.random.poisson(curLambda / rangesScale, clients);
-            
+            clientsInterFlow = generateVectorMessages(lambdas[curLambda], clients);
+
             if (sum(clientsInterFlow) > 1):
                 lost += 1;
 
@@ -32,7 +44,7 @@ def solve(slots, clients):
 
 slots = 1000; # количество слотов для моделирования
 
-examples = [3, 5, 15]; # количество абонентов, варианты для моделирования
+examples = [2, 3, 5, 15, 50]; # количество абонентов, варианты для моделирования
 
 plt.figure();
 for i in range(len(examples)):
