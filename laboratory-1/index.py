e9d7e6c3437144dcc5b1aab3cadb2a64ef49a51f
Vladislav Veluga <vladislav805@yandex.com>
2020-03-05 19:16:23 +0300

    Основное задание примерно так

--- laboratory-1/index.py
+++ laboratory-1/index.py
@@ -3,50 +3,46 @@ from matplotlib import pyplot as plt;
 import random;
 
 clients = 100; # N, кол-во абонентов
-slots = 1000; # количество слотов для моделирования
-messages = [];
-neededClient = 0;
+slots = 5000; # количество слотов для моделирования
+client = 3; # наблюдаемый клиент
 
-lambdas = [1] #[i / 100 for i in range(5, 100)];
+lambdas = [i / 10000 for i in range(1, 150)];
 
-neededClient = 1;
+def getInterflow(p, n):
+    res = [];
+    for i in range(n):
+        res.append(1 if random.random() <= p else 0);
+    return res;
 
-data = [
-    1 for i in range(len(lambdas))
-];
+def pushBuffers(buffer, interflow, i):
+    buffer.append(buffer[i - 1] + interflow[i]);
 
-def getInterflow(_lambda, n):
-    return np.random.poisson(_lambda, n);
-
-def pushBuffers(buffers, interflow):
-    for i in range(buffers):
-        buffers[i] += interflow[i];
-    return buffers;
-
-def popMessage(buffers, client):
-    if (buffers[client] > 0):
-        buffers[client] -= 1;
-    return buffers;
+def popMessage(buffer, interflow, i):
+    if (buffer[i - 1] > 0):
+        buffer[i] -= 1;
 
 def solve(slots, clients):
     graph = []; # данные для графика
-    for curLambda in range(len(lambdas)):
-        buffers = [0 for i in range(clients)]; # буферы всех абонентов
+    print(len(graph));
+    for i in range(len(lambdas)):
+        curLambda = lambdas[i];
 
-        for slot in range(slots):
-            interflow = getInterflow(curLambda, clients); # генерация сообщений у каждого клиента
-            print(interflow);
-            break;
-            popMessage(buffers, slot % clients);
+        interflow = getInterflow(curLambda, slots); # генерация сообщений
 
-        break;
-        graph.append(clients * sum(buffers) / slots);
+        buffer = [interflow[0]]; # размер буфера в определённом слоте
+
+        for slot in range(1, slots):
+            pushBuffers(buffer, interflow, slot);
+            if slot % clients == client:
+                popMessage(buffer, interflow, slot);
+
+        graph.append(clients * sum(buffer) / slots);
+        del buffer;
 
     return graph;
 
-plt.title('test');
-plt.xlabel('p');
-plt.ylabel('Messages');
+plt.xlabel('Вероятность успешной отправки');
+plt.ylabel('Среднее число сообщений в слоте');
 plt.plot(lambdas, solve(slots, 50));
 plt.grid();
 plt.show();
